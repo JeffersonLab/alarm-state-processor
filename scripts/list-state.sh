1e744189d38b7829451a2e91d32edf0c6c93461a
ryans <ryans@jlab.org>
2021-03-25 16:41:55 -0400

    Script to query AlarmState

---
+++ scripts/list-state.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+[ -z "$BOOTSTRAP_SERVERS" ] && echo "BOOTSTRAP_SERVERS environment required" && exit 1;
+
+CWD=$(readlink -f "$(dirname "$0")")
+
+APP_HOME=$CWD/..
+
+FILTER_JAR=`ls $APP_HOME/lib/alarms-filter*`
+CLIENTS_JAR=`ls $APP_HOME/lib/kafka-clients-*`
+JACK_CORE=`ls $APP_HOME/lib/jackson-core-*`
+JACK_BIND=`ls $APP_HOME/lib/jackson-databind-*`
+JACK_ANN=`ls $APP_HOME/lib/jackson-annotations-*`
+SLF4J_API=`ls $APP_HOME/lib/slf4j-api-*`
+SLF4J_IMP=`ls $APP_HOME/lib/slf4j-log4j*`
+LOG4J_IMP=`ls $APP_HOME/lib/log4j-*`
+
+RUN_CP="$FILTER_JAR:$CLIENTS_JAR:$SLF4J_API:$SLF4J_IMP:$LOG4J_IMP:$LOG4J_CONF:$JACK_CORE:$JACK_BIND:$JACK_ANN"
+
+java -Dlog.dir=$APP_HOME/logs -Dlog4j.configuration="file://$APP_HOME/config/log4j-client.properties" -cp $RUN_CP org.jlab.jaws.client.AlarmStateConsumer $BOOTSTRAP_SERVERS alarm-state
---
+++ src/main/java/org/jlab/jaws/org/jlab/jaws/client/AlarmStateConsumer.java
@@ -0,0 +1,49 @@
+package org.jlab.jaws.org.jlab.jaws.client;
+
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
+import org.jlab.jaws.eventsource.EventSourceConfig;
+import org.jlab.jaws.eventsource.EventSourceListener;
+import org.jlab.jaws.eventsource.EventSourceRecord;
+import org.jlab.jaws.eventsource.EventSourceTable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Properties;
+
+public class AlarmStateConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(AlarmStateConsumer.class);
+
+    public static void main(String[] args) throws InterruptedException {
+        final String servers = args[0];
+
+        final Properties props = new Properties();
+
+        props.put(EventSourceConfig.EVENT_SOURCE_TOPIC, "alarm-state");
+        props.put(EventSourceConfig.EVENT_SOURCE_BOOTSTRAP_SERVERS, servers);
+        props.put(EventSourceConfig.EVENT_SOURCE_KEY_DESERIALIZER, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(EventSourceConfig.EVENT_SOURCE_VALUE_DESERIALIZER, "org.apache.kafka.common.serialization.StringDeserializer");
+
+        // Deserializer specific configs
+        props.put(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://registry:8081");
+        props.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG,"true");
+
+        final EventSourceTable<String, String> consumer = new EventSourceTable<>(props);
+
+        consumer.addListener(new EventSourceListener<>() {
+            @Override
+            public void update(List<EventSourceRecord<String, String>> changes) {
+                for (EventSourceRecord<String, String> record : changes) {
+                    String key = record.getKey();
+                    String value = record.getValue();
+                    System.out.println(key + "=" + value);
+                }
+                consumer.close();
+            }
+        });
+
+        consumer.start();
+        consumer.join(); // block until first update, which contains current state of topic
+    }
+}
