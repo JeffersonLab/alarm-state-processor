eddc781e7c5a69bdd9bb3349f8af4982df0a8464
ryans <ryans@jlab.org>
2021-05-11 14:02:22 -0400

    Use AVRO for state value

--- scripts/list-state.sh
+++ scripts/list-state.sh
@@ -15,7 +15,16 @@ JACK_ANN=`ls $APP_HOME/lib/jackson-annotations-*`
 SLF4J_API=`ls $APP_HOME/lib/slf4j-api-*`
 SLF4J_IMP=`ls $APP_HOME/lib/slf4j-log4j*`
 LOG4J_IMP=`ls $APP_HOME/lib/log4j-*`
+EXTRA=`ls $APP_HOME/lib/kafka-avro-serializer-*`
+EXTRA2=`ls $APP_HOME/lib/common-utils-*`
+EXTRA3=`ls $APP_HOME/lib/kafka-schema-registry-client-*`
+EXTRA4=`ls $APP_HOME/lib/kafka-schema-serializer-*`
+EXTRA5=`ls $APP_HOME/lib/avro-*`
+EXTRA6=`ls $APP_HOME/lib/jakarta.ws.rs-api-*`
+EXTRA7=`ls $APP_HOME/lib/jersey-common-*`
+STREAMS1=`ls $APP_HOME/lib/kafka-streams-2.*.jar`
+STREAMS2=`ls $APP_HOME/lib/kafka-streams-avro-serde-*`
 
-RUN_CP="$APP_JAR:$JAWS_JAR:$CLIENTS_JAR:$SLF4J_API:$SLF4J_IMP:$LOG4J_IMP:$LOG4J_CONF:$JACK_CORE:$JACK_BIND:$JACK_ANN"
+RUN_CP="$APP_JAR:$JAWS_JAR:$CLIENTS_JAR:$SLF4J_API:$SLF4J_IMP:$LOG4J_IMP:$LOG4J_CONF:$JACK_CORE:$JACK_BIND:$JACK_ANN:$EXTRA:$EXTRA2:$EXTRA3:$EXTRA4:$EXTRA5:$EXTRA6:$EXTRA7:$STREAMS1:$STREAMS2"
 
 java -Dlog.dir=$APP_HOME/logs -Dlog4j.configuration="file://$APP_HOME/config/log4j-client.properties" -cp $RUN_CP org.jlab.jaws.client.AlarmStateConsumer $BOOTSTRAP_SERVERS alarm-state
--- src/main/java/org/jlab/jaws/client/AlarmStateConsumer.java
+++ src/main/java/org/jlab/jaws/client/AlarmStateConsumer.java
@@ -1,6 +1,8 @@
 package org.jlab.jaws.client;
 
 import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import org.jlab.jaws.entity.AlarmStateValue;
 import org.jlab.jaws.eventsource.EventSourceConfig;
 import org.jlab.jaws.eventsource.EventSourceListener;
 import org.jlab.jaws.eventsource.EventSourceRecord;
@@ -8,7 +10,6 @@ import org.jlab.jaws.eventsource.EventSourceTable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
 import java.util.Properties;
 import java.util.Set;
 
@@ -21,23 +22,25 @@ public class AlarmStateConsumer {
 
         final Properties props = new Properties();
 
+        final SpecificAvroSerde<AlarmStateValue> VALUE_SERDE = new SpecificAvroSerde<>();
+
         props.put(EventSourceConfig.EVENT_SOURCE_TOPIC, "alarm-state");
         props.put(EventSourceConfig.EVENT_SOURCE_BOOTSTRAP_SERVERS, servers);
         props.put(EventSourceConfig.EVENT_SOURCE_KEY_DESERIALIZER, "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put(EventSourceConfig.EVENT_SOURCE_VALUE_DESERIALIZER, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(EventSourceConfig.EVENT_SOURCE_VALUE_DESERIALIZER, VALUE_SERDE.deserializer().getClass().getName());
 
         // Deserializer specific configs
         props.put(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://registry:8081");
         props.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG,"true");
 
-        final EventSourceTable<String, String> consumer = new EventSourceTable<>(props);
+        final EventSourceTable<String, AlarmStateValue> consumer = new EventSourceTable<>(props);
 
         consumer.addListener(new EventSourceListener<>() {
             @Override
-            public void initialState(Set<EventSourceRecord<String, String>> records) {
-                for (EventSourceRecord<String, String> record : records) {
+            public void initialState(Set<EventSourceRecord<String, AlarmStateValue>> records) {
+                for (EventSourceRecord<String, AlarmStateValue> record : records) {
                     String key = record.getKey();
-                    String value = record.getValue();
+                    AlarmStateValue value = record.getValue();
                     System.out.println(key + "=" + value);
                 }
                 consumer.close();
